7d547b283177024fd338a47d3f6b823eca92be68
m19e <nexus.me7290@gmail.com>
2020-08-16 21:04:36 +0900

    Make mock-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface ITodo {
+    id: number;
+    content: string;
 }
+
+const App = () => {
+    const [value, setValue] = useState("");
+    const [todoList, setTodoList] = useState([]);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log("change process", e);
+    };
+
+    const add = () => {
+        console.log("add process");
+    };
+
+    const handleDelete = (id: number) => {
+        console.log("delete process");
+    };
+
+    return (
+        <div>
+            <h1>TODO app</h1>
+            <input type="text" value={value} onChange={handleChange} />
+            <button onClick={add}>Add</button>
+            <ul>
+                {todoList.map((todo: ITodo) => (
+                    <li key={todo.id}>
+                        {todo.content}
+                        <button onClick={() => handleDelete(todo.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
