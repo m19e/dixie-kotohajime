06823133fc0c7a0c1a241a8c5e6764231cc4a10e
m19e <nexus.me7290@gmail.com>
2020-08-25 14:30:59 +0900

    Remove needless state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -59,34 +59,6 @@ const App = () => {
     //     { title: "Chicken", children: [{ title: "Egg" }] },
     //     { title: "Fish", children: [{ title: "fingerline" }] },
     // ]);
-    const [treeData, setTreeData] = useState<any[]>([
-        { title: ".gitignore" },
-        { title: "package.json" },
-        {
-            title: "src",
-            isDirectory: true,
-            expanded: true,
-            children: [{ title: "styles.css" }, { title: "index.js" }, { title: "reducers.js" }, { title: "actions.js" }, { title: "utils.js" }],
-        },
-        {
-            title: "tmp",
-            isDirectory: true,
-            children: [{ title: "12214124-log" }, { title: "drag-disabled-file", dragDisabled: true }],
-        },
-        {
-            title: "build",
-            isDirectory: true,
-            children: [{ title: "react-sortable-tree.js" }],
-        },
-        {
-            title: "public",
-            isDirectory: true,
-        },
-        {
-            title: "node_modules",
-            isDirectory: true,
-        },
-    ]);
 
     useEffect(() => {
         // loadListFromDB("todos", setTodoList);
