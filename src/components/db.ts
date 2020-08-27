33510849b5987037f1ab3dfd4379ea458972d462
m19e <nexus.me7290@gmail.com>
2020-08-27 15:48:49 +0900

    Remove tree table

--- src/components/db.ts
+++ src/components/db.ts
@@ -1,6 +1,6 @@
 import Dexie from "dexie";
 
 const db = new Dexie("ReactSampleDB");
-db.version(1).stores({ todos: "++id", tree: "++id" });
+db.version(1).stores({ todos: "++id" });
 
 export default db;
