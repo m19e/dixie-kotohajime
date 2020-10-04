a2dc90732e0664f6dce937af8420a6ac32e51d98
m19e <nexus.me7290@gmail.com>
2020-10-04 19:27:59 +0900

    Comment-out old process

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -169,109 +169,109 @@ const App = () => {
         setEditMode(false);
     };
 
-    const createNodeProps = (rowInfo: ExtendedNodeData): { [index: string]: JSX.Element[] } => {
-        if (rowInfo.node.isEditting)
-            return {
-                icons: rowInfo.node.isDir
-                    ? [
-                          <div
-                              style={{
-                                  borderLeft: "solid 8px gray",
-                                  borderBottom: "solid 10px gray",
-                                  marginRight: 10,
-                                  boxSizing: "border-box",
-                                  width: 16,
-                                  height: 12,
-                                  filter: rowInfo.node.expanded
-                                      ? "drop-shadow(1px 0 0 gray) drop-shadow(0 1px 0 gray) drop-shadow(0 -1px 0 gray) drop-shadow(-1px 0 0 gray)"
-                                      : "none",
-                                  borderColor: rowInfo.node.expanded ? "white" : "gray",
-                              }}
-                          />,
-                      ]
-                    : [],
-                title: [<input type="text" value={value} onChange={handleChange}></input>],
-                buttons: rowInfo.node.isNewFile
-                    ? [
-                          <button onClick={() => updateNode(rowInfo, { title: value, isEditting: false, isNewFile: false })}>Add</button>,
-                          <button onClick={() => deleteNode(rowInfo)}>Cancel</button>,
-                      ]
-                    : [
-                          <button onClick={() => updateNode(rowInfo, { title: value, isEditting: false })}>Update</button>,
-                          <button onClick={() => updateNode(rowInfo, { isEditting: false })}>Cancel</button>,
-                      ],
-            };
+    // const createNodeProps = (rowInfo: ExtendedNodeData): { [index: string]: JSX.Element[] } => {
+    //     if (rowInfo.node.isEditting)
+    //         return {
+    //             icons: rowInfo.node.isDir
+    //                 ? [
+    //                       <div
+    //                           style={{
+    //                               borderLeft: "solid 8px gray",
+    //                               borderBottom: "solid 10px gray",
+    //                               marginRight: 10,
+    //                               boxSizing: "border-box",
+    //                               width: 16,
+    //                               height: 12,
+    //                               filter: rowInfo.node.expanded
+    //                                   ? "drop-shadow(1px 0 0 gray) drop-shadow(0 1px 0 gray) drop-shadow(0 -1px 0 gray) drop-shadow(-1px 0 0 gray)"
+    //                                   : "none",
+    //                               borderColor: rowInfo.node.expanded ? "white" : "gray",
+    //                           }}
+    //                       />,
+    //                   ]
+    //                 : [],
+    //             title: [<input type="text" value={value} onChange={handleChange}></input>],
+    //             buttons: rowInfo.node.isNewFile
+    //                 ? [
+    //                       <button onClick={() => updateNode(rowInfo, { title: value, isEditting: false, isNewFile: false })}>Add</button>,
+    //                       <button onClick={() => deleteNode(rowInfo)}>Cancel</button>,
+    //                   ]
+    //                 : [
+    //                       <button onClick={() => updateNode(rowInfo, { title: value, isEditting: false })}>Update</button>,
+    //                       <button onClick={() => updateNode(rowInfo, { isEditting: false })}>Cancel</button>,
+    //                   ],
+    //         };
 
-        if (editMode)
-            return {
-                icons: rowInfo.node.isDir
-                    ? [
-                          <div
-                              style={{
-                                  borderLeft: "solid 8px gray",
-                                  borderBottom: "solid 10px gray",
-                                  marginRight: 10,
-                                  boxSizing: "border-box",
-                                  width: 16,
-                                  height: 12,
-                                  filter: rowInfo.node.expanded
-                                      ? "drop-shadow(1px 0 0 gray) drop-shadow(0 1px 0 gray) drop-shadow(0 -1px 0 gray) drop-shadow(-1px 0 0 gray)"
-                                      : "none",
-                                  borderColor: rowInfo.node.expanded ? "white" : "gray",
-                              }}
-                          />,
-                      ]
-                    : [],
-            };
+    //     if (editMode)
+    //         return {
+    //             icons: rowInfo.node.isDir
+    //                 ? [
+    //                       <div
+    //                           style={{
+    //                               borderLeft: "solid 8px gray",
+    //                               borderBottom: "solid 10px gray",
+    //                               marginRight: 10,
+    //                               boxSizing: "border-box",
+    //                               width: 16,
+    //                               height: 12,
+    //                               filter: rowInfo.node.expanded
+    //                                   ? "drop-shadow(1px 0 0 gray) drop-shadow(0 1px 0 gray) drop-shadow(0 -1px 0 gray) drop-shadow(-1px 0 0 gray)"
+    //                                   : "none",
+    //                               borderColor: rowInfo.node.expanded ? "white" : "gray",
+    //                           }}
+    //                       />,
+    //                   ]
+    //                 : [],
+    //         };
 
-        return {
-            icons: rowInfo.node.isDir
-                ? [
-                      <div
-                          style={{
-                              borderLeft: "solid 8px gray",
-                              borderBottom: "solid 10px gray",
-                              marginRight: 10,
-                              boxSizing: "border-box",
-                              width: 16,
-                              height: 12,
-                              filter: rowInfo.node.expanded
-                                  ? "drop-shadow(1px 0 0 gray) drop-shadow(0 1px 0 gray) drop-shadow(0 -1px 0 gray) drop-shadow(-1px 0 0 gray)"
-                                  : "none",
-                              borderColor: rowInfo.node.expanded ? "white" : "gray",
-                          }}
-                      />,
-                  ]
-                : [],
-            buttons: rowInfo.node.isDir
-                ? [
-                      <button
-                          onClick={() => {
-                              setEditMode(true);
-                              setValue(rowInfo.node.title as string);
-                              updateNode(rowInfo, { isEditting: true });
-                          }}
-                      >
-                          Rename
-                      </button>,
-                      <button onClick={() => createNode(rowInfo, false)}>AddFile</button>,
-                      <button onClick={() => createNode(rowInfo, true)}>AddDir</button>,
-                      <button onClick={() => deleteNode(rowInfo)}>Delete</button>,
-                  ]
-                : [
-                      <button
-                          onClick={() => {
-                              setEditMode(true);
-                              setValue(rowInfo.node.title as string);
-                              updateNode(rowInfo, { isEditting: true });
-                          }}
-                      >
-                          Rename
-                      </button>,
-                      <button onClick={() => deleteNode(rowInfo)}>Delete</button>,
-                  ],
-        };
-    };
+    //     return {
+    //         icons: rowInfo.node.isDir
+    //             ? [
+    //                   <div
+    //                       style={{
+    //                           borderLeft: "solid 8px gray",
+    //                           borderBottom: "solid 10px gray",
+    //                           marginRight: 10,
+    //                           boxSizing: "border-box",
+    //                           width: 16,
+    //                           height: 12,
+    //                           filter: rowInfo.node.expanded
+    //                               ? "drop-shadow(1px 0 0 gray) drop-shadow(0 1px 0 gray) drop-shadow(0 -1px 0 gray) drop-shadow(-1px 0 0 gray)"
+    //                               : "none",
+    //                           borderColor: rowInfo.node.expanded ? "white" : "gray",
+    //                       }}
+    //                   />,
+    //               ]
+    //             : [],
+    //         buttons: rowInfo.node.isDir
+    //             ? [
+    //                   <button
+    //                       onClick={() => {
+    //                           setEditMode(true);
+    //                           setValue(rowInfo.node.title as string);
+    //                           updateNode(rowInfo, { isEditting: true });
+    //                       }}
+    //                   >
+    //                       Rename
+    //                   </button>,
+    //                   <button onClick={() => createNode(rowInfo, false)}>AddFile</button>,
+    //                   <button onClick={() => createNode(rowInfo, true)}>AddDir</button>,
+    //                   <button onClick={() => deleteNode(rowInfo)}>Delete</button>,
+    //               ]
+    //             : [
+    //                   <button
+    //                       onClick={() => {
+    //                           setEditMode(true);
+    //                           setValue(rowInfo.node.title as string);
+    //                           updateNode(rowInfo, { isEditting: true });
+    //                       }}
+    //                   >
+    //                       Rename
+    //                   </button>,
+    //                   <button onClick={() => deleteNode(rowInfo)}>Delete</button>,
+    //               ],
+    //     };
+    // };
 
     return (
         <div>
